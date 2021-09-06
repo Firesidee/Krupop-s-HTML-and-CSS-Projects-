7f4dd5495dfd8b0f4a468e46fd9f4105de1e9cbd
Firesidee <KathrynY@protonmail.com>
2021-09-06 13:19:59 -0600

    one page website

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 function My_First_Function() {
     var String = "Kiss me, I'm Irish!";
     var result = String.fontcolor("green");
     document.getElementById("Irish").innerHTML = 
     result;
 }
+=======
+function My_First_Function() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = 
+    result;
+}
+>>>>>>> 54ad4305c4a981c772607564269c7f45160b6a1a
